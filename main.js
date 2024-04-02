57ab883086ef9a880733c6432faf3e2087c8bf49
for best <105922773+kokushibu@users.noreply.github.com>
2024-04-02 09:28:09 -0300

    k

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const botoes = document.querySelectorAll(".botao")
-const textos = document.querySelectorAll(".abas-conteudo")
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".abas-conteudo");
+
 for (let i=0; i < botoes.length; i++){
 botoes[i].onclick = function(){
     for (let j = 0; j< botoe.length; j++){
-    botoe[j].clasList.remove("ativo");
+    botoes[j].clasList.remove("ativo");
     textos[j].clasList.remove("ativo");
     }
     botoes[i].classList.add("ativo");
