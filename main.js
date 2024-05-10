8e9bcdcc879f4a63709e07c16e554a7ea3ea9658
for best <105922773+kokushibu@users.noreply.github.com>
2024-05-10 10:09:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,36 @@ botoes[i].onclick = function(){
     textos[i].classList.add("ativo");
   }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-28T:23:59:59");
+const tempoObjetivo2 = new Date("2024-07-05T:23:59:59");
+const tempoObjetivo3 = new Date("2024-08-01T:23:59:59");
+const tempoObjetivo4 = new Date("2024-12-01T:23:59:59");
+
+const tempos = [
+  tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+]
+
+for (let i=0; i< contadores.length; i++){
+  contadores[i].textContent = calculaTempo(tempos[i]);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo1){
+  let tempoAtual1 = new Date();
+  let tempoFinal = tempoObjetivo1 - tempoAtual1;
+  let segundos = Math.floor(tempoFinal /1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+
+return dias + "dias"
++horas + "horas"
++minutos + "minutos"
++segundos + "segundos";
+
+}
